5f8c119ace1c74ccf8d04f7d8032c43590b0ea92
Ph4ra0hXX <ph4ra0hx@gmail.com>
2024-05-05 08:20:33 -0300

    fix map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from '../components/map/map.component';
+import { InputSearchComponent } from '../components/input-search/input-search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, MapComponent, InputSearchComponent],
 })
 export class AppComponent {
   title = 'ajuda-rs-front';
